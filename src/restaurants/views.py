eee0d1615237f1b9cdcdaf91bbd3b2f7236e63cf
eeq <eeq@absolute-performance.com>
2017-12-22 15:14:08 -0400

    Added templates folder and update the project config file.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,18 +1,10 @@
+import random
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 # function based view
 def home(request):
-	html_var = 'f strings'
-	html_ = f"""
-	<html lang=en>
-	<head></head>
-	<body>
-		<h1>Hello World</h1>
-		<p>This is {html_var} coming through</p>
-	</body>
-	</html>
-	"""
-	return HttpResponse(html_)
-	#return render(request, "home.html", {}) #response
+	num = random.randint(0, 100000000)
+	return render(request, "base.html", {'html_var': True, 'num': num})
