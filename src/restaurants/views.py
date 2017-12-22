d9bceba24aa974ebc0692f2faaca9b586d44c60d
eeq <eeq@absolute-performance.com>
2017-12-22 11:32:25 -0400

    Adding Restaurants app for test the template rendering.

---
+++ src/restaurants/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+# function based view
+def home(request):
+	html_var = 'f strings'
+	html_ = f"""
+	<html lang=en>
+	<head></head>
+	<body>
+		<h1>Hello World</h1>
+		<p>This is {html_var} coming through</p>
+	</body>
+	</html>
+	"""
+	return HttpResponse(html_)
+	#return render(request, "home.html", {}) #response
